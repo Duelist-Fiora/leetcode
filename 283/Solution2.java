677e87af5006152f89f39bffe13046009e487900
liangjh <liangjh@iaskdoc.com>
2018-08-02 14:03:47 +0800

    283

--- 283/Solution2.java
+++ 283/Solution2.java
@@ -1,9 +1,18 @@
-package PACKAGE_NAME;
-
 /**
  * @author liangjh
  * @create 2018-08-02 14:02
  **/
 public class Solution2 {
-
+    public void moveZeros(int[] nums) {
+        int j = 0;
+        for (int i = 0; i < nums.length; i++){
+            if (nums[i] != 0) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        for (;j < nums.length; j++) {
+            nums[j] = 0;
+        }
+    }
 }
